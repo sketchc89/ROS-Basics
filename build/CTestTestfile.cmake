9362cb98da89c1763479f5acc2a09c08ba2c3fac
Chris Sketch <sketchc89@gmail.com>
2017-07-31 20:48:36 -0500

    Basic counting node

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sketchc89/rosbook/src
+# Build directory: /home/sketchc89/rosbook/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(basics)
