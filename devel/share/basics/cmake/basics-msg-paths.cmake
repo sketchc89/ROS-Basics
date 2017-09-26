513a8d09b2c3a20074942464f1917e940ed8b81f
Chris Sketch <christopher.sketch@gm.com>
2017-09-26 17:13:14 -0400

    Added r2 robot packages

--- devel/share/basics/cmake/basics-msg-paths.cmake
+++ devel/share/basics/cmake/basics-msg-paths.cmake
@@ -1,4 +1,4 @@
 # generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
 
-set(basics_MSG_INCLUDE_DIRS "/home/sketchc89/rosbook/src/basics/msg;/home/sketchc89/rosbook/devel/share/basics/msg")
+set(basics_MSG_INCLUDE_DIRS "/home/bz6w8b/projects/ROS-Basics/src/basics/msg;/home/bz6w8b/projects/ROS-Basics/devel/share/basics/msg")
 set(basics_MSG_DEPENDENCIES std_msgs;actionlib_msgs)
---
+++ devel/share/nasa_r2_common_msgs/cmake/nasa_r2_common_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(nasa_r2_common_msgs_MSG_INCLUDE_DIRS "/home/bz6w8b/projects/ROS-Basics/src/deprecated_nasa_r2_common/nasa_r2_common_msgs/msg")
+set(nasa_r2_common_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs;visualization_msgs;trajectory_msgs)
